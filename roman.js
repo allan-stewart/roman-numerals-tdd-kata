440dbe2a9719f408796bbed465cd66aa5867a15f
Allan Stewart <allan-stewart@pluralsight.com>
2016-03-03 22:56:13 -0700

    Make test for D pass

--- roman.js
+++ roman.js
@@ -1,5 +1,6 @@
 exports.toRoman = function (input) {
   var mapping = [
+    {numeral: 'D', value: 500},
     {numeral: 'CD', value: 400},
     {numeral: 'C', value: 100},
     {numeral: 'XC', value: 90},
