11b8b1a8c59ffbf4263808b31bac9515ad5e4cfe
Allan Stewart <allan-stewart@pluralsight.com>
2016-03-03 21:52:50 -0700

    Add first failing test

--- test/roman-tests.js
+++ test/roman-tests.js
@@ -3,6 +3,9 @@ var assert = require('assert');
 
 describe('roman', function () {
   describe('toRoman', function () {
-    
+    it('should convert 1 to I', function () {
+      var result = roman.toRoman(1);
+      assert.equal(result, 'I');
+    });
   });
 });
