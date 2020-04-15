6a19114780cf4985b6aaa8598ff1a941c75bc368
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-15 20:40:47 +0700

    Add conversion for only one symbol for I,V,X

--- roman.js
+++ roman.js
@@ -1,3 +1,19 @@
-exports.toRoman = function (input) {
-  return '';
+exports.toRoman = (input) => {
+  if (input === 1) {
+    return "I";
+  } else if (input == 2) {
+    return "II";
+  } else if (input === 3) {
+    return "III";
+  }
+
+  if (input === 10) {
+    return "X";
+  } else if (input === 20) {
+    return "XX";
+  } else if (input === 30) {
+    return "XXX";
+  }
+
+  return "V";
 };
--- test/roman.test.js
+++ test/roman.test.js
@@ -1,7 +1,51 @@
-var roman = require('../roman.js');
+var roman = require("../roman.js");
 
-describe('roman', function () {
-  test('toRoman', function () {
-    expect(1).toBe(1);
+describe("roman", () => {
+  describe("only I symbol", () => {
+    test("should convert 1 to I", () => {
+      var result = roman.toRoman(1);
+
+      expect(result).toBe("I");
+    });
+
+    test("should convert 2 to II", () => {
+      var result = roman.toRoman(2);
+
+      expect(result).toBe("II");
+    });
+
+    test("should convert 3 to III", () => {
+      var result = roman.toRoman(3);
+
+      expect(result).toBe("III");
+    });
+  });
+
+  describe("only V symbol", () => {
+    test("should convert 5 to V", () => {
+      var result = roman.toRoman(5);
+
+      expect(result).toBe("V");
+    });
+  });
+
+  describe("only X symbol", () => {
+    test("should convert 10 to X", () => {
+      var result = roman.toRoman(10);
+
+      expect(result).toBe("X");
+    });
+
+    test("should convert 20 to XX", () => {
+      var result = roman.toRoman(20);
+
+      expect(result).toBe("XX");
+    });
+
+    test("should convert 30 to XXX", () => {
+      var result = roman.toRoman(30);
+
+      expect(result).toBe("XXX");
+    });
   });
 });
