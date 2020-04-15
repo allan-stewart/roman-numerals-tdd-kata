0b3115b6407089de8120758a129967f9111c3699
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-15 21:01:54 +0700

    Refactor Extract function for each satisfaction ccondition for symbol

--- roman.js
+++ roman.js
@@ -1,20 +1,50 @@
 exports.toRoman = (input) => {
-  var result = "";
+  let result = "";
 
-  while (input >= 10) {
+  while (canUseX(input)) {
     result += "X";
     input -= 10;
   }
 
-  if (input >= 5) {
+  if (canUseIX(input)) {
+    result += "IX";
+    input -= 9;
+  }
+
+  if (canUseV(input)) {
     result += "V";
     input -= 5;
-  } 
+  }
 
-  while (input >= 1) {
+  if (canUseIV(input)) {
+    result += "IV";
+    input -= 4;
+  }
+
+  while (canUseI(input)) {
     result += "I";
     input -= 1;
   }
 
   return result;
 };
+
+const canUseI = (input) => {
+  return input >= 1;
+};
+
+function canUseIV(input) {
+  return input === 4;
+}
+
+function canUseIX(input) {
+  return input === 9;
+}
+
+function canUseX(input) {
+  return input >= 10;
+}
+
+function canUseV(input) {
+  return input >= 5;
+}
--- test/roman.test.js
+++ test/roman.test.js
@@ -1,21 +1,21 @@
-var roman = require("../roman.js");
+const roman = require("../roman.js");
 
 describe("roman", () => {
   describe("only I symbol", () => {
     test("should convert 1 to I", () => {
-      var result = roman.toRoman(1);
+      const result = roman.toRoman(1);
 
       expect(result).toBe("I");
     });
 
     test("should convert 2 to II", () => {
-      var result = roman.toRoman(2);
+      const result = roman.toRoman(2);
 
       expect(result).toBe("II");
     });
 
     test("should convert 3 to III", () => {
-      var result = roman.toRoman(3);
+      const result = roman.toRoman(3);
 
       expect(result).toBe("III");
     });
@@ -23,7 +23,7 @@ describe("roman", () => {
 
   describe("only V symbol", () => {
     test("should convert 5 to V", () => {
-      var result = roman.toRoman(5);
+      const result = roman.toRoman(5);
 
       expect(result).toBe("V");
     });
@@ -31,19 +31,19 @@ describe("roman", () => {
 
   describe("only X symbol", () => {
     test("should convert 10 to X", () => {
-      var result = roman.toRoman(10);
+      const result = roman.toRoman(10);
 
       expect(result).toBe("X");
     });
 
     test("should convert 20 to XX", () => {
-      var result = roman.toRoman(20);
+      const result = roman.toRoman(20);
 
       expect(result).toBe("XX");
     });
 
     test("should convert 30 to XXX", () => {
-      var result = roman.toRoman(30);
+      const result = roman.toRoman(30);
 
       expect(result).toBe("XXX");
     });
@@ -84,6 +84,38 @@ describe("roman", () => {
       const result = roman.toRoman(38);
 
       expect(result).toBe('XXXVIII');
+    });
+  });
+
+  describe('multiple symbols with subtraction only', () => {
+    test('should convert 4 to IV', () => {
+      const result = roman.toRoman(4);
+
+      expect(result).toBe('IV');
+    });
+
+    test('should convert 14 to XIV', () => {
+      const result = roman.toRoman(14);
+
+      expect(result).toBe('XIV');
+    });
+
+    test('should convert 34 to XIV', () => {
+      const result = roman.toRoman(34);
+
+      expect(result).toBe('XXXIV');
+    });
+
+    test('should convert 9 to IX', () => {
+      const result = roman.toRoman(9);
+
+      expect(result).toBe('IX');
+    });
+
+    test('should convert 39 to XXXIX', () => {
+      const result = roman.toRoman(39);
+
+      expect(result).toBe('XXXIX');
     });
   });
 });
